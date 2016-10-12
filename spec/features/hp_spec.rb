a0500f296f64cd0770702bbe19301239f9cbee58
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-12 12:21:30 +0100

    Add HP value for p2

--- spec/features/hp_spec.rb
+++ spec/features/hp_spec.rb
@@ -1,12 +1,12 @@
 require './app'
 require 'spec_helper'
 
-feature "Enter names do" do
-  scenario "fill in form" do
+feature "show hit points" do
+  scenario "show Player 2's hit points" do
     visit "/"
     fill_in "P1Name", with: "Nicole"
     fill_in "P2Name", with: "Asif"
     click_button "Submit"
-    expect(page).to have_text "Nicole vs. Asif"
+    expect(page).to have_text "Asif = 100HP"
   end
 end
